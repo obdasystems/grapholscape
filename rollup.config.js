e4a56f0b0c80850b93295f948092d4d73515c188
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2019-10-07 09:38:28 +0200

    restore initial rollup.config and add polyfills for old browsers

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,54 @@
-import resolve from 'rollup-plugin-node-resolve'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import license from 'rollup-plugin-license'
 import path from 'path'
 import json from 'rollup-plugin-json'
+
+const VERSION = process.env.VERSION || 'snapshot' // default snapshot
+const FILE = process.env.FILE
+const SOURCEMAPS = process.env.SOURCEMAPS === 'true' // default false
+const BABEL = process.env.BABEL !== 'false' // default true
+const NODE_ENV = process.env.NODE_ENV === 'development' ? 'development' : 'production' // default prod
+const matchSnapshot = process.env.SNAPSHOT === 'match'
+
+const input = './src/grapholscape.js'
+const name = 'grapholscape'
+
+const envVariables = {
+  'process.env.VERSION': JSON.stringify(VERSION),
+  'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+}
+
+const getJsonOptions = () => ({
+  include: 'src/**',
+  exclude: [ '**./**' ],
+
+  // for tree-shaking, properties will be declared as
+  // variables, using either `var` or `const`
+  preferConst: true, // Default: false
+
+  // specify indentation for the generated default export —
+  // defaults to '\t'
+  indent: '  ',
+
+  // ignores indent and generates the smallest code
+  compact: true, // Default: false
+
+  // generate a named export for every property of the JSON object
+  namedExports: true // Default: true
+})
+
+const getBabelOptions = () => ({
+  exclude: '**/node_modules/**',
+  externalHelpers: true
+})
+
+// Ignore all node_modules dependencies
+const isExternal = id => !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/')
 
 const licenseHeaderOptions = {
   sourcemap: true,
@@ -12,38 +57,47 @@ const licenseHeaderOptions = {
   }
 }
 
-export default {
-  // If using any exports from a symlinked project, uncomment the following:
-  // preserveSymlinks: true,
-	input: ['./src/grapholscape.js'],
-	output: {
-		file: 'build/grapholscape.js',
-    format: 'umd',
-    name: 'grapholscape',
-		sourcemap: true
-	},
-	plugins: [
-    json({
-      include: 'src/**',
-      exclude: [ '**./**' ],
+const configs = [
+  {
+    input,
+    output: {
+      file: 'build/grapholscape.js',
+      format: 'umd',
+      name,
+      sourcemap: SOURCEMAPS ? 'inline' : false
+    },
+    plugins: [
+      json(getJsonOptions()),
+      nodeResolve(),
+      commonjs({ include: '**/node_modules/**' }),
+      BABEL ? babel(getBabelOptions()) : {},
+      replace(envVariables),
+      license(licenseHeaderOptions),
+      !FILE ? sizeSnapshot({ matchSnapshot }) : {}
+    ]
+  },
 
-      // for tree-shaking, properties will be declared as
-      // variables, using either `var` or `const`
-      preferConst: true, // Default: false
+  {
+    input,
+    output: {
+      file: 'build/grapholscape.min.js',
+      format: 'umd',
+      name
+    },
+    plugins: [
+      json(getJsonOptions()),
+      nodeResolve(),
+      commonjs({ include: '**/node_modules/**' }),
+      BABEL ? babel(getBabelOptions()) : {},
+      replace(envVariables),
+      terser({
+        sourcemap: false
+      }),
+      license(licenseHeaderOptions)
+    ]
+  }
+]
 
-      // specify indentation for the generated default export —
-      // defaults to '\t'
-      indent: '  ',
-
-      // ignores indent and generates the smallest code
-      compact: true, // Default: false
-
-      // generate a named export for every property of the JSON object
-      namedExports: true // Default: true
-    }),
-    resolve(),
-    commonjs({ include: '**/node_modules/**' }),
-    babel({ exclude: '**/node_modules/**' }),
-    license(licenseHeaderOptions)
-  ]
-};
+export default FILE
+  ? configs.filter(config => config.output.file.includes(FILE))
+  : configs
