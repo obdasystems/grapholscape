9ad4d7799edf3210cb0c54137a7f17951e90b7ad
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2019-11-02 19:15:06 +0100

    init electron-app - intro mockup

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('app/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
