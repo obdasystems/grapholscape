e472a843771ff5c1409e7e21fc3192a373b9e34b
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2021-11-12 09:01:09 +0100

    Update exports (full and bare)

--- src/index.js
+++ src/index.js
@@ -11,9 +11,48 @@ import Grapholscape from './grapholscape'
 cytoscape.use(popper)
 cytoscape.use(cola)
 
-export default function (file, container, config = null) {
-  if (!container) return undefined
+//export { GrapholscapeRenderer } from './rendering/renderers'
 
+/**
+ * Create an instance of Grapholscape with complete GUI
+ * @param {string | object} file the ontology, can be an object of the 
+ * [Web API interface File](https://developer.mozilla.org/en-US/docs/Web/API/File) 
+ * or a String representing the .graphol file to be displayed
+ * @param {object} container a DOM element in which the ontology will be rendered in
+ * @param {object} config a config object, please read more about [settings](https://github.com/obdasystems/grapholscape/wiki/Settings)
+ * @returns {Promise<Grapholscape>} a promise that will be fulfilled with a Grapholscape object
+ */
+export async function fullGrapholscape(file, container, config = null) {
+  if (!file || !container) {
+    console.error('Please specify an ontology and a container for Grapholscape')
+    return undefined
+  }
+
+  const grapholscape = await getPromise(file, container, config)
+  initGUI(grapholscape)
+  return grapholscape
+}
+
+/**
+ * Create an instance of Grapholscape with complete GUI
+ * @param {string | object} file the ontology, can be an object of the 
+ * [Web API interface File](https://developer.mozilla.org/en-US/docs/Web/API/File) 
+ * or a String representing the .graphol file to be displayed
+ * @param {object} container a DOM element in which the ontology will be rendered in
+ * @param {object} config a config object, please read more about [settings](https://github.com/obdasystems/grapholscape/wiki/Settings)
+ * @returns {Promise<Grapholscape>} a promise that will be fulfilled with a Grapholscape object
+ */
+export function bareGrapholscape(file, container, config = null) {
+  if (!file || !container) {
+    console.error('Please specify an ontology and a container for Grapholscape')
+    return undefined
+  }
+
+  const promise = getPromise(file, container, config)
+  return promise
+}
+
+function getPromise(file, container, config) {
   return new Promise ((resolve, reject) => {
     let ontology = null
 
@@ -42,13 +81,10 @@ export default function (file, container, config = null) {
 
     function init() {
       try {
-        let grapholscape = new Grapholscape(ontology, container, config)
-        initGUI(grapholscape)
-        resolve(grapholscape)
+        resolve(new Grapholscape(ontology, container, config))
       } catch (e) { console.log(e)}
     }
-  })
-  .catch(error => console.log(error) )
+  }).catch(error => console.log(error) )
 
   function getResult(file) {
     return new GrapholParser(file).parseGraphol()
