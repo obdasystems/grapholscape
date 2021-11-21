02b694784a0da20b1868dbdb0a09782bab5e2af5
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2021-11-21 10:13:55 +0100

    fix widget drag

--- src/ui/common/gscape-widget.js
+++ src/ui/common/gscape-widget.js
@@ -240,6 +240,7 @@ export default class GscapeWidget extends LitElement {
 
     function dragMouseDown(e) {
       e = e || window.event
+      e.preventDefault()
       // get the mouse cursor position at startup:
       pos3 = e.clientX
       pos4 = e.clientY
