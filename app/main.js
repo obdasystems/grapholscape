66c60156e04628a5b3e2682b5d973df7b16afb33
gianluca-pepe <37369540+gianluca-pepe@users.noreply.github.com>
2021-12-06 17:07:47 +0100

    [desktop-app] upload app using preload.js

--- app/main.js
+++ app/main.js
@@ -2,47 +2,56 @@
 const { app, BrowserWindow, ipcMain , dialog, nativeImage} = require('electron')
 const fs = require('fs')
 const path = require('path')
+const recentsManager = require('./recent-files-manager')
+
+recentsManager.readRecentFiles()
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let introWindow
 let gscapeWindow
 const appIcon = nativeImage.createFromPath(path.resolve(__dirname, 'assets', 'icon-128.png'))
 
-function createWindow () {
+function createIntroWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  introWindow = new BrowserWindow({
     width: 750,
     height: 600,
     show: false,
-    webPreferences: {nodeIntegration: true},
     autoHideMenuBar: true,
     resizable: false,
-    icon: appIcon
+    icon: appIcon,
+    webPreferences: {
+      preload: path.join(__dirname, 'intro', 'preload.js')
+    },
+    title: 'Grapholscape Desktop'
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.resolve(__dirname, 'intro', 'index.html'))
+  introWindow.loadFile(path.resolve(__dirname, 'intro', 'index.html'))
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  introWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    introWindow = null
   })
 
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
+  introWindow.once('ready-to-show', () => {
+    introWindow.show()
   })
+
+  introWindow.webContents.send('version', app.getVersion())
+  introWindow.webContents.send('recents-update', recentsManager.recents)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createIntroWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -56,15 +65,17 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (introWindow === null) {
+    createIntroWindow()
   }
 })
 
 function useGrapholscape(file) {
   gscapeWindow = new BrowserWindow({
     show: false,
-    webPreferences: {nodeIntegration: true},
+    webPreferences: {
+      preload: path.join(__dirname, 'graphol', 'preload.js')
+    },
     autoHideMenuBar: true,
     icon: appIcon
   })
@@ -72,22 +83,33 @@ function useGrapholscape(file) {
   gscapeWindow.maximize()
   gscapeWindow.loadFile(path.resolve(__dirname, 'graphol','grapholscape-window.html'))
   gscapeWindow.webContents.on('did-finish-load', () => {
-    gscapeWindow.webContents.send('start', file.string)
+    gscapeWindow.webContents.send('start', file.string, file.grapholPath)
   })
 
   gscapeWindow.on('closed', () => {
     gscapeWindow = null
-    mainWindow.show()
+    introWindow.show()
   })
 
 }
 
+/**
+ * invoked by intro renderer process when user select a file to be used
+ * either from the recently used files list or dropping a file in the 
+ * drag-drop box
+ */
 ipcMain.handle('use-graphol-path', (e, grapholPath) => {
+  recentsManager.logRecentFile(grapholPath)
   getFileString(grapholPath, useGrapholscape)
+  return recentsManager.recents
 })
 
+/**
+ * invoked by intro renderer process when user click on drag-drop box
+ * for selecting a file from a dialog
+ */
 ipcMain.handle('select-file', async e => {
-  const result = await dialog.showOpenDialog({
+  await dialog.showOpenDialog({
     title: 'Seleziona un File .graphol',
     properties : ['openFile'],
     filters: [
@@ -96,18 +118,43 @@ ipcMain.handle('select-file', async e => {
     ],
   }).then( result => {
     if (!result.canceled) {
+      recentsManager.logRecentFile(result.filePaths[0])
       getFileString(result.filePaths[0], useGrapholscape)
-      return result.filePaths[0]
     }
-    else return null
-  })
+  }).catch( reason => console.error(reason))
 
-  return result
+  return recentsManager.recents
 })
 
+/**
+ * invoked by intro renderer process when user delete a recently used file
+ */
+ipcMain.handle('delete-recent', (e, grapholPath) => {
+  recentsManager.deleteRecentFile(grapholPath)
+  return recentsManager.recents
+})
+
+/**
+ * invoked by grapholscape renderer process when initialisation has finished
+ * and grapholscape is ready to be used
+ */
 ipcMain.on('gscape-ready', () => {
   gscapeWindow.show()
-  mainWindow.hide()
+  introWindow.hide()
+})
+
+ipcMain.on('gscape-error', (e, filePath, error) => {
+  dialog.showMessageBox({
+    type: 'error',
+    title: 'Unable to use selected file',
+    message: 'Selected file cannot be used, please check it\'s of the right format and of an admitted Graphol version (ie. > 2)\n\
+    Error: '+error,
+    buttons: ['OK'],
+  }).then( () => {
+    recentsManager.deleteRecentFile(filePath)
+    introWindow.webContents.send('recents-update', recentsManager.recents)
+    gscapeWindow.close()
+  })
 })
 
 function getFileString(grapholPath, callback) {
@@ -117,13 +164,14 @@ function getFileString(grapholPath, callback) {
         dialog.showMessageBox({
           type: 'warning',
           title: 'Warning',
-          message: 'Selected file does not exist, it will be removed from the list.',
+          message: 'Selected file does not exist anymore, it will be removed from the list.',
           buttons: ['OK'],
         }).then( () => {
-          mainWindow.webContents.send('remove-recent', grapholPath)
+          recentsManager.deleteRecentFile(grapholPath)
+          introWindow.webContents.send('recents-update', recentsManager.recents)
         })
       }
-      console.log("An error ocurred reading the file :" + err.message);
+      console.error("An error ocurred reading the file :" + err.message);
         return;
     }
 
