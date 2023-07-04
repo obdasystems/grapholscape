d63c0b1c03536f4c91dbd0e6ab0ac034e0af8b5e
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2023-07-04 12:51:56 +0200

    Fix typeorversion in ontologyViewData

--- src/ui/util/get-ontology-view-data.ts
+++ src/ui/util/get-ontology-view-data.ts
@@ -4,7 +4,7 @@ import { OntologyViewModel } from "../ontology-info/ontology-info"
 export default function ontologyModelToViewData(ontologyModelData: Ontology) {
     let ontologyViewData: OntologyViewModel = {
         name: ontologyModelData.name,
-        typeOrVersion: ontologyModelData.version,
+        typeOrVersion: new Set([ontologyModelData.version]),
         iri: ontologyModelData.iri || '',
         namespaces: ontologyModelData.namespaces,
         annotations: ontologyModelData.annotations,
